19000eeff32b9294f8cb454d60772d728eb1fdce
Kevin Lam <kevinlam@Kevins-MacBook-Pro.local>
2018-01-18 10:04:45 -0800

    Passes the Prototypal queue and stack sprints

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,40 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-};
+  var someInstance = Object.create(queueMethods);
+  someInstance.count = 0;
+  someInstance.storage = {};
+  someInstance.first = 0;
+  someInstance.last = 0;
+  return someInstance;
+}; 
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(val) {
+    if (this.count === 0) {
+      this.storage[this.last] = val;
+    } else {
+      this.last++;
+      this.storage[this.last] = val;
+    }
+    this.count++;
+  }, 
+  dequeue: function() {
+    var deletedVal = this.storage[this.first];
+    if (this.count === 1) {
+      delete this.storage[this.first];
+      this.count--;
+      return deletedVal;
+    } else if (this.count > 1) {
+      delete this.storage[this.first];
+      this.count--;
+      this.first++;
+      return deletedVal;
+    }
+  }, 
+  size: function() {
+    return this.count;
+  }
+};
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,29 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  someInstance.count = 0;
+  someInstance.storage = {};
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(val) {
+    this.storage[this.count] = val;
+    console.log(this.storage);
+    this.count++;
+  }, 
+  pop: function() {
+    if (this.count > 0) {
+      var deletedValue = this.storage[this.count - 1];
+      delete this.storage[this.count - 1];
+      this.count--; 
+      return deletedValue;
+    }
+  }, 
+  size: function() {
+    return this.count; 
+  }
+};
 
 
