bd15388d1a39eb28012ac932f8ab9154ed564384
Kevin Lam <kevinlam@Kevins-MacBook-Pro.local>
2018-01-18 10:38:56 -0800

    Passes ES6 Queue

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,36 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.counter = 0;
+    this.first = 0;
+    this.last = 0;
+  }
+
+  enqueue(val) {
+    if (this.counter === 0) {
+      this.storage[this.counter] = val; 
+    } else {
+      this.last++;
+      this.storage[this.last] = val; 
+    }
+    this.counter++;
+  }
+  dequeue() {
+    var deletedVal = this.storage[this.first];
+    if (this.counter === 1) {
+      delete this.storage[this.first];
+      this.counter--;
+      return deletedVal;
+    } else if (this.counter > 1) {
+      delete this.storage[this.first];
+      this.counter--;
+      this.first++;
+      return deletedVal;
+    }
+  }
+  size() {
+    return this.counter;
   }
 
 }
