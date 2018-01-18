17621dafbb593ffdcb2bb8af607f1efe58f75fa5
Kevin Lam <kevinlam@Kevins-MacBook-Pro.local>
2018-01-18 10:05:36 -0800

    Passes Pseudoclassical Stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,22 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.counter = 0;
 };
 
-
+Stack.prototype.push = function(val) {
+  this.storage[this.counter] = val;
+  this.counter++;
+}, 
+Stack.prototype.pop = function() {
+  if (this.counter > 0) {
+    var deletedVal = this.storage[this.counter - 1];
+    delete this.storage[this.counter - 1];
+    this.counter--;
+    return deletedVal;
+  }
+}, 
+Stack.prototype.size = function() {
+  return this.counter;
+};
