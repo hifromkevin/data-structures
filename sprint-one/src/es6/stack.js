7c486a8d76f8c7f580fde5746f9e0236d1b2bee9
Kevin Lam <kevinlam@Kevins-MacBook-Pro.local>
2018-01-18 10:32:42 -0800

    Passes ES6 Stack

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,25 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.counter = 0;
+    this.storage = {};
+  }
+
+  push(val) {
+    this.storage[this.counter] = val;
+    this.counter++;
+  }
+  pop() {
+    if (this.counter > 0) {
+      var deletedValue = this.storage[this.counter - 1];
+      delete this.storage[this.counter - 1];
+      this.counter--; 
+      return deletedValue;
+    }
+  }
+  size() {
+    return this.counter;
   }
 
 }
+
