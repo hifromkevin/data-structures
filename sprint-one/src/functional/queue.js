91b56c69957ea6ed814f9d861f6bd94e7efcc9fa
Kevin Lam <kevinlam@Kevins-MacBook-Pro.local>
2018-01-17 16:36:20 -0800

    Passes functional queue and stack including linter error fixes

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,39 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count   = 0;
+  var first   = 0;
+  var last    = 0;
 
   // Implement the methods below
 
+
   someInstance.enqueue = function(value) {
+    if (count === 0) {
+      storage[last] = value;
+    } else {
+      last++;
+      storage[last] = value;
+    }
+    count++;
   };
 
   someInstance.dequeue = function() {
+    var deletedVal = storage[first];
+    if (count === 1) {
+      delete storage[first];
+      count--;
+      return deletedVal;
+    } else if (count > 1) {
+      delete storage[first];
+      count--;
+      first++;
+      return deletedVal;
+    }
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
